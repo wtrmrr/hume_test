981438f24326e24912dfd2aae5b823414c607cda
wtrmrr <69556131+wtrmrr@users.noreply.github.com>
2025-06-22 22:40:33 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/Nav.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { useLayoutEffect, useState } from "react";
+import HumeLogo from "./logos/Hume";
+import { Button } from "./ui/button";
+import { Moon, Sun } from "lucide-react";
+import Github from "./logos/GitHub";
+import pkg from '@/package.json';
+
+export const Nav = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useLayoutEffect(() => {
+    const el = document.documentElement;
+
+    if (el.classList.contains("dark")) {
+      setIsDarkMode(true);
+    } else {
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDark = () => {
+    const el = document.documentElement;
+    el.classList.toggle("dark");
+    setIsDarkMode((prev) => !prev);
+  };
+
+  return (
+    <div
+      className={
+        "fixed top-0 right-0 px-4 py-2 flex items-center h-14 z-50"
+      }
+    >
+      <div className={"ml-auto flex items-center gap-1"}>
+        <Button
+          onClick={() => {
+            window.open(
+              pkg.homepage,
+              "_blank",
+              "noopener noreferrer"
+            );
+          }}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5 rounded-full"}
+        >
+          <span>
+            <Github className={"size-4"} />
+          </span>
+          <span>Star on GitHub</span>
+        </Button>
+        <Button
+          onClick={toggleDark}
+          variant={"ghost"}
+          className={"ml-auto flex items-center gap-1.5 rounded-full"}
+        >
+          <span>
+            {isDarkMode ? (
+              <Sun className={"size-4"} />
+            ) : (
+              <Moon className={"size-4"} />
+            )}
+          </span>
+          <span>{isDarkMode ? "Light" : "Dark"} Mode</span>
+        </Button>
+      </div>
+    </div>
+  );
+};
